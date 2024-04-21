b671cb0c433a6ff54e645ead6e7680bf727e7f69
Mrinal Raj <rajvermacasual@gmail.com>
2024-04-21 19:16:12 +0530

    Added favourable nifty all stocks from 2017

--- python/src/generate_today_buy_sell_stocks.py
+++ python/src/generate_today_buy_sell_stocks.py
@@ -34,8 +34,8 @@ def get_backtest_start_end_date():
     backtest_start_date = one_month_ago.strftime("%Y-%m-%d")
     backtest_end_date = current_date.strftime("%Y-%m-%d")
 
-    # backtest_start_date = "2023-01-01"
-    # backtest_end_date = "2023-11-21"
+    # backtest_start_date = "2024-01-01"
+    # backtest_end_date = "2024-04-03" # For backtesting add 1 day + the actual date
     return backtest_start_date, backtest_end_date
 
 def create_threads_to_do_transactions(ticker_name, ticker_data, sell_column, buy_columns_combinations):
